c04efa637974335cb592d986a3cc3f37db971c29
olet <olet@163.com>
2024-05-04 21:31:21 +0800

    test

--- include/ui/CmdLaunch.h
+++
@@ -1,9 +0,0 @@
-#ifndef CMDLAUNCH_H_
-#define CMDLAUNCH_H_
-
-class NemesisInfo;
-
-void CmdGenerateInitialize(VecStr modlist, const NemesisInfo* nemesisInfo);
-void CmdUpdateInitialize(const NemesisInfo* nemesisInfo);
-
-#endif
